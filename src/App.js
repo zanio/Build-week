0604d12c9978be3320f2da7c70f4af9a74f825f1
Zanio <akp.ani@yahoo.com>
2019-09-16 04:58:19 +0100

    chore(react-project-setup):setup project to use react [Start]

---
+++ src/App.js
@@ -0,0 +1,7 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import './app.css';
+
+const App = () => <div className="app">Welcome Home</div>;
+
+export default App;
