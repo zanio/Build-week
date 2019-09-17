ffb7850bda79201a2bbcad25b5ecdbe61e6f746f
Zanio <akp.ani@yahoo.com>
2019-09-17 18:08:13 +0100

    chore(finish-touch):complete style feed page

--- src/components/feed/feed.js
+++ src/components/feed/feed.js
@@ -13,7 +13,8 @@ console.log(getDayTime())
 return(
     <React.Fragment>
         <div className={classes.Feed}>
-            <div>Day 27th Facts in Sept, 2019</div>
+            <div className={classes.triangle}></div>
+            <div className={classes.day}>Day 27th Facts in Sept, 2019</div>
             <p>In relation to their body size, cats have the largest eyes of any mammal</p>
         </div>
     </React.Fragment>
--- src/components/footer/social/social.js
+++ src/components/footer/social/social.js
@@ -1,9 +1,6 @@
 import React, { Component }  from 'react';
 import classes from '../footer.css';
 
-import SVG from 'react-inlinesvg';
-import facebook from '../../../assets/icons/facebook.svg'
-import twitter from '../../../assets/icons/twitter.svg'
 
 class SocialMedia extends Component{
     render(){
@@ -11,8 +8,12 @@ class SocialMedia extends Component{
             <React.Fragment>
             <div>
             <p>Follow me on social media for more fun Application</p>
-            <div className={classes.responsive}>
-            <span><SVG src={facebook} /> <SVG src={twitter} /></span>
+            <div>
+            <span className={classes.icon}>
+                <a href="https://web.facebook.com/akpan.aniefiok.90"><i class="fab fa-facebook-square"></i></a>
+                <a href="https://twitter.com/NodeDeWeb"><i class="fab fa-twitter"></i></a>
+                <a href="https://github.com/zanio"><i class="fab fa-github"></i></a>
+                </span>
             </div>
                 </div>
             </React.Fragment>
