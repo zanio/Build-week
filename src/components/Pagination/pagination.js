3812a270dfe8198f4cd225ba51275a14b8042fe2
Zanio <akp.ani@yahoo.com>
2019-09-19 16:33:48 +0100

    chore(pagination):edit pagination page

--- src/components/Pagination/pagination.js
+++ src/components/Pagination/pagination.js
@@ -11,7 +11,7 @@ const pagesCount = Math.ceil(itemsCount / pageSize) ;
 if(pagesCount === 1) return null;
 const pages = _.range(1, pagesCount + 1)
 
-    return <nav className="width-20">
+    return <nav>
         <ul className = "pagination">
     {pages.map(page => (
         <li key = {page} className={page === currentPage ? "page-item active":"page-item"}>
